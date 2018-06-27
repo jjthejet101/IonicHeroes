1aa584451c17d26627bc247004eb6fd527938bc7
Justin Jetton <justinkjetton@gmail.com>
2018-06-26 23:58:36 -0500

    data bindings and created class

--- MyIonicTourHeroes/src/pages/home/home.ts
+++ MyIonicTourHeroes/src/pages/home/home.ts
@@ -1,6 +1,11 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+export class Hero {
+  id: number;
+  name: string;
+}
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
@@ -11,4 +16,6 @@ export class HomePage {
 
   }
 
+  title = 'Tour of Heroes';
+  hero = 'Windstorm';
 }
