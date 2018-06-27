7d9f3fdd6ea00e283168c76ded883a99f3af66e4
Justin Jetton <justinkjetton@gmail.com>
2018-06-27 10:44:11 -0500

    created array

--- MyIonicTourHeroes/src/pages/home/home.ts
+++ MyIonicTourHeroes/src/pages/home/home.ts
@@ -6,16 +6,26 @@ export class Hero {
   name: string;
 }
 
+const HEROES: Hero[] = [
+  { id: 11, name: 'Mr. Nice' },
+  { id: 12, name: 'Narco' },
+  { id: 13, name: 'Bombasto' },
+  { id: 14, name: 'Celeritas' },
+  { id: 15, name: 'Magneta' },
+  { id: 16, name: 'RubberMan' },
+  { id: 17, name: 'Dynama' },
+  { id: 18, name: 'Dr IQ' },
+  { id: 19, name: 'Magma' },
+  { id: 20, name: 'Tornado' }
+];
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-
   constructor(public navCtrl: NavController) {
-
   }
-  
   title = 'Tour of Heroes';
   hero: Hero = {
     id: 1,
